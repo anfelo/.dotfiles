0b2049343ede397918f55f961ad846d9d83e7e1e
Andres Osorio <af.osorio1341@gmail.com>
2023-12-31 09:33:11 -0500

    make cmp window bordered

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
+    {
         "nvim-treesitter/nvim-treesitter-context",
-        "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    build = ":TSUpdate",
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
 }
