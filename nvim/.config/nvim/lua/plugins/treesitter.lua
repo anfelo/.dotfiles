96390654436cd329fee8b3b0f62beadf6914c666
Andres Osorio <af.osorio1341@gmail.com>
2024-02-06 09:17:33 +0100

    initialize treesitter context and treesitter textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,18 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup()
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
         dependencies = {
             "nvim-treesitter/nvim-treesitter",
-        }
+        },
+        config = function()
+            require("nvim-treesitter.configs").setup({})
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter",
