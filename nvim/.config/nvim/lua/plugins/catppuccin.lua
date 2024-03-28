4165f23d2c02a1d6c954537d3e03baeedf53ef77
Andres Osorio <af.osorio1341@gmail.com>
2024-03-28 23:14:46 +0100

    use catppuccin theme

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,21 @@
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "mocha", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+            transparent_background = false, -- Enable this to disable setting the background color
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                mason = true,
+            },
+        })
+
+        vim.o.termguicolors = true
+        vim.cmd.colorscheme("catppuccin")
+    end,
+}
